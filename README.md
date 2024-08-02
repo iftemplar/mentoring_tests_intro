1aca5210b63d99d5855dfacad75a2ca139be3d1f
Sandrina Pereira <a.sandrina.p@gmail.com>
2024-08-02 14:38:06 +0100

    better instructions

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # tests_from_sandbox
+
 Created with CodeSandbox
 
 ## Intructions
 
 1. Clone the repo
 2. Install dependencies
-3. Run the project with npm run dev
+3. Run the project with `npm run dev`
+4. Follow the app instructions.
+5. Run tests with `npm run test` or `npm t`
--- src/index.js
+++ src/index.js
@@ -1,50 +1,40 @@
-import './styles.css';
+import "./styles.css";
 
-import { multiply, isSmallerThan10 } from './1_basics';
-import { guardMove } from './2_mockFn';
-import { generatePassword } from './3_mockDep';
-import { fetchJoke } from './4_mockFetch';
+import { multiply, isSmallerThan10 } from "./1_basics";
+import { guardMove } from "./2_mockFn";
+import { generatePassword } from "./3_mockDep";
+import { fetchJoke } from "./4_mockFetch";
 
-document.getElementById('app').innerHTML = `
+document.getElementById("app").innerHTML = `
 <h1>Intro to Testing with Jest</h1>
 <div>
-  Playground to practice Jest. 
+  <p>You can see all functions being called in this page.</p>
 
-  You can see all functions being called in this page.
-
-  <br/>
-
-  Instructions:
-  <ol>
-    <li>Fork this sandbox</li>
-    <li>Click the Github icon in the sidebar to create a repo</li>
-    <li>In the repo, create a new branch</li>
-    <li>Write the tests there and send me the PR.</li>
-  </ol>  
+  <p>Write the missing tests and open one or multiple PRs as you progress.</p>
 </div>
 `;
 
 async function init() {
-  console.log('multiply', multiply(3, 4));
+  console.log("multiply", multiply(3, 4));
 
-  console.log('isSmallerThan10 - 1', isSmallerThan10(1));
-  console.log('isSmallerThan10 - 20', isSmallerThan10(20));
-
-  guardMove(() => {
-    console.log('Im walking!');
-  }, 'walk');
+  console.log("isSmallerThan10 - 1", isSmallerThan10(1));
+  console.log("isSmallerThan10 - 20", isSmallerThan10(20));
 
   guardMove(() => {
-    console.log('I was not logged.');
-  }, 'swim');
+    console.log("Im walking!");
+  }, "walk");
+
+  guardMove(() => {
+    console.log("I was not logged.");
+  }, "swim");
 
   console.log(await generatePassword(5));
-  console.log(await generatePassword(5, 'aaa'));
+  console.log(await generatePassword(5, "aaa"));
 
   // Random joke
   console.log(await fetchJoke());
   // Jokes that contain "what"
-  console.log(await fetchJoke('what'));
+  console.log(await fetchJoke("what"));
 }
 
 init();
