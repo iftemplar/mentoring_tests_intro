aebc1f36fb009ee7d3da45711d9b01fcad499339
iftemplar <samila.mykola@gmail.com>
2024-10-25 21:53:15 +0200

    Test v2 cosmetic updates

--- src/2_mockFn.test.js
+++ src/2_mockFn.test.js
@@ -2,16 +2,15 @@ import { guardMove } from "./2_mockFn";
 
 describe("Moving a Guard", () => {
   test("Sends a move or run action to the guard", () => {
-    const realFuntion = (value) => {
+    const testFunction = (value) => {
       if (["walk", "run"].includes(value)) {
         return value;
       }
     };
 
-    const shadowCallback = jest.fn(realFuntion);
-
-    const result = guardMove(shadowCallback, "walk", 7);
-    expect(shadowCallback).toHaveBeenCalledTimes(1);
+    const mockCallback = jest.fn(testFunction); // create a mock fn
+    const result = guardMove(mockCallback, "walk", 7);
+    expect(mockCallback).toHaveBeenCalledTimes(1);
     expect(result).toBe("walk");
   });
 });
